00a6aecc8257884a9a75e98c2fb9c0648b09a248
natin56y <32836662+natin56y@users.noreply.github.com>
2020-02-13 00:35:57 +0100

    feature: added the nodejs server with express and mangoose

--- angularClient/src/environments/environment.ts
+++ angularClient/src/environments/environment.ts
@@ -1,9 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { } from 'node';
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: process.env.API_URL || 'http://127.0.0.1:8080'
 };
 
 /*
---
+++ nodeServer/routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+exports = module.exports = function(app) {
+
+    app.get('/hello', (req, res) => res.send('Hello World!'))
+}

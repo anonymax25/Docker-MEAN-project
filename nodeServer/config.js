4637ec3cd41aa38dc71dab2c968accd812ff8d26
natin56y <32836662+natin56y@users.noreply.github.com>
2020-02-14 19:24:23 +0100

    testing API to connect it in a docker 2

--- nodeServer/config.js
+++ nodeServer/config.js
@@ -2,8 +2,8 @@
 return module.exports = config = {
     mongo: {
         uri: process.env.MONGO_URL ||
-        'mongodb://127.0.0.1/mean-app',
-        options: []
+        'mongodb://127.0.0.1/app',
+        options: [useNewUrlParser: true]
     },
     port: process.env.port ||
     '8080'
